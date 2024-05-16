a8ada9f51d9e5f201c0fe16e4a58492af57c201c
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-05-16 15:58:34 +0200

    Add styling to chat window

--- common/chatWindow.py
+++ common/chatWindow.py
@@ -13,35 +13,37 @@ class ChatWindow(Window):
     def __init__(self, closeCallback: Callable[[], Any], socket: Socket, sendCallback: Callable[[Socket, tkt.Entry], None]) -> None:
         # Builds the underlying window
         Window.__init__(self, CHAT_WINDOW_DIMS[0], CHAT_WINDOW_DIMS[1], CHAT_WINDOW_TITLE, closeCallback)
+        # Set the window's background
+        self.window.configure(bg=BACKGROUND_COLOR)
         # Configuration of the grid for the window 
         self.window.grid_columnconfigure(0, weight=1)
         self.window.grid_rowconfigure(0, weight=1)
         # Base panel to hold all the different widgets
-        panelChat: tkt.Frame = tkt.Frame(self.window)
+        panelChat: tkt.Frame = tkt.Frame(self.window, bg=BACKGROUND_COLOR)
         panelChat.grid(column=0, row=0, sticky='nsew', padx=4, pady=4)
         # Configuration of the grid for the panel 
         panelChat.grid_columnconfigure(0, weight=1)
         panelChat.grid_rowconfigure(0, weight=1)
         panelChat.grid_rowconfigure(1, weight=1)
         # First panel to hold the chat screen and input
-        panelChatText: tkt.Frame = tkt.Frame(panelChat)
+        panelChatText: tkt.Frame = tkt.Frame(panelChat, bg=BACKGROUND_COLOR)
         panelChatText.grid(column=0, row=0, sticky='sew', padx=4, pady=4)
         # Configuration of the grid for the text panel
         panelChatText.grid_columnconfigure(0, weight=1)
         panelChatText.grid_rowconfigure(0, weight=1)
         panelChatText.grid_rowconfigure(1, weight=1)
         # Second panel to hold the two buttons
-        panelChatButtons: tkt.Frame = tkt.Frame(panelChat)
+        panelChatButtons: tkt.Frame = tkt.Frame(panelChat, bg=BACKGROUND_COLOR)
         panelChatButtons.grid(column=0, row=1, sticky='new', padx=4, pady=4)
         # Configuration of the grid for the button panel
         panelChatButtons.grid_rowconfigure(0, weight=1)
         panelChatButtons.grid_columnconfigure(0, weight=1)
         panelChatButtons.grid_columnconfigure(1, weight=1)
         # Listbox to show the chat's messages
-        self.messageList: tkt.Listbox = tkt.Listbox(panelChatText, height=15, yscrollcommand=tkt.Scrollbar(panelChatText).set)
+        self.messageList: tkt.Listbox = tkt.Listbox(panelChatText, borderwidth=0, highlightthickness=0, fg=TEXT_COLOR, bg=SECONDARY_COLOR ,yscrollcommand=tkt.Scrollbar(panelChatText).set)
         self.messageList.grid(column=0, row=0, sticky='new', padx=2, pady=2)
         # Message input as an entry 
-        self.messageIn: tkt.Entry = tkt.Entry(panelChatText)
+        self.messageIn: tkt.Entry = tkt.Entry(panelChatText, borderwidth=0, highlightthickness=0, fg=TEXT_COLOR, bg=SECONDARY_COLOR)
         self.messageIn.insert(0, "Inserisci il messaggio")
         self.messageIn.bind('<FocusIn>', lambda e: self.entryInCallback(self.messageIn, "Inserisci il messaggio"))
         self.messageIn.bind('<FocusOut>', lambda e: self.entryOutCallback(self.messageIn, "Inserisci il messaggio"))
@@ -49,8 +51,12 @@ class ChatWindow(Window):
         self.messageIn.grid(column=0, row=1, padx=2, pady=2, sticky='sew')
         self.messageIn.focus()
         # Button to quit the chat 
-        buttonQuit: tkt.Button = tkt.Button(panelChatButtons, text="Quit", command=self.close)
+        buttonQuit: tkt.Button = tkt.Button(panelChatButtons, text="Quit", borderwidth=0, highlightthickness=0, command=self.close, fg=TEXT_COLOR, bg=PRIMARY_COLOR)
+        buttonQuit.bind("<Enter>", lambda _e: buttonQuit.configure(fg=TEXT_COLOR, bg=ACCENT_COLOR))
+        buttonQuit.bind("<Leave>", lambda _e: buttonQuit.configure(fg=TEXT_COLOR, bg=PRIMARY_COLOR))
         buttonQuit.grid(column=0, row=0, padx=2, pady=2, sticky='nsw')
         # Button to send the message
-        buttonSend: tkt.Button = tkt.Button(panelChatButtons, text="Send", command=lambda: sendCallback(socket, self.messageIn))
+        buttonSend: tkt.Button = tkt.Button(panelChatButtons, text="Send", borderwidth=0, highlightthickness=0, command=lambda: sendCallback(socket, self.messageIn), fg=TEXT_COLOR, bg=PRIMARY_COLOR)
+        buttonSend.bind("<Enter>", lambda _e: buttonSend.configure(fg=TEXT_COLOR, bg=ACCENT_COLOR))
+        buttonSend.bind("<Leave>", lambda _e: buttonSend.configure(fg=TEXT_COLOR, bg=PRIMARY_COLOR))
         buttonSend.grid(column=1, row=0, padx=2, pady=2, sticky='nse')
